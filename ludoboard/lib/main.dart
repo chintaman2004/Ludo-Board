8b8151855e9379ba44a016a370e0920cf40b8174
chintaman2004 <ahmed.hassanc@gmail.com>
2025-06-04 16:25:10 +0500

    Errors REmoved

--- ludoboard/lib/main.dart
+++ ludoboard/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(LudoBoardApp());
+void main() {
+  runApp(LudoBoardApp());
+}
 
 class LudoBoardApp extends StatelessWidget {
   const LudoBoardApp({super.key});
@@ -8,61 +10,124 @@ class LudoBoardApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Custom Ludo Board',
       home: Scaffold(
-        appBar: AppBar(title: Text('15x15 Ludo Board')),
-        body: LudoBoard(),
+        appBar: AppBar(title: Text("15x15 Boxes Grid")),
+        body: Column(
+          children: [
+            Row(children: createBoxes()),
+            Row(children: createBoxes()),
+            Row(children: createBoxes()),
+            Row(children: createBoxes()),
+            Row(children: createBoxes()),
+            Row(children: createBoxes()),
+            Row(children: createBoxes()),
+            Row(children: createBoxes()),
+            Row(children: createBoxes()),
+            Row(children: createBoxes()),
+            Row(children: createBoxes()),
+            Row(children: createBoxes()),
+            Row(children: createBoxes()),
+            Row(children: createBoxes()),
+            Row(children: createBoxes()),
+          ],
+        ),
       ),
     );
   }
-}
 
-class LudoBoard extends StatelessWidget {
-  final int gridSize = 15;
-
-  final List<List<Color>> colorGrid = List.generate(15, (row) {
-    return List.generate(15, (col) {
-      if (row < 6 && col < 6) return Colors.green.shade300;
-      if (row < 6 && col > 8) return Colors.red.shade300;
-      if (row > 8 && col < 6) return Colors.blue.shade300;
-      if (row > 8 && col > 8) return Colors.yellow.shade300;
-      if (row == 7 || col == 7) return Colors.grey.shade300;
-      return Colors.white;
-    });
-  });
-
-  LudoBoard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        double boardSize = constraints.maxWidth;
-        return Center(
-          child: SizedBox(
-            width: boardSize,
-            height: boardSize, // Ensures square board
-            child: Column(
-              children: List.generate(gridSize, (row) {
-                return Expanded(
-                  child: Row(
-                    children: List.generate(gridSize, (col) {
-                      return Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black12),
-                            color: colorGrid[row][col],
-                          ),
-                        ),
-                      );
-                    }),
-                  ),
-                );
-              }),
-            ),
-          ),
-        );
-      },
-    );
+  // Function to create 15 boxes manually
+  List<Widget> createBoxes() {
+    return [
+      Container(
+        width: 30,
+        height: 30,
+        color: Colors.red,
+        margin: EdgeInsets.all(2),
+      ),
+      Container(
+        width: 30,
+        height: 30,
+        color: Colors.blue,
+        margin: EdgeInsets.all(2),
+      ),
+      Container(
+        width: 30,
+        height: 30,
+        color: Colors.green,
+        margin: EdgeInsets.all(2),
+      ),
+      Container(
+        width: 30,
+        height: 30,
+        color: Colors.yellow,
+        margin: EdgeInsets.all(2),
+      ),
+      Container(
+        width: 30,
+        height: 30,
+        color: Colors.purple,
+        margin: EdgeInsets.all(2),
+      ),
+      Container(
+        width: 30,
+        height: 30,
+        color: Colors.orange,
+        margin: EdgeInsets.all(2),
+      ),
+      Container(
+        width: 30,
+        height: 30,
+        color: Colors.pink,
+        margin: EdgeInsets.all(2),
+      ),
+      Container(
+        width: 30,
+        height: 30,
+        color: Colors.cyan,
+        margin: EdgeInsets.all(2),
+      ),
+      Container(
+        width: 30,
+        height: 30,
+        color: Colors.teal,
+        margin: EdgeInsets.all(2),
+      ),
+      Container(
+        width: 30,
+        height: 30,
+        color: Colors.lime,
+        margin: EdgeInsets.all(2),
+      ),
+      Container(
+        width: 30,
+        height: 30,
+        color: Colors.indigo,
+        margin: EdgeInsets.all(2),
+      ),
+      Container(
+        width: 30,
+        height: 30,
+        color: Colors.brown,
+        margin: EdgeInsets.all(2),
+      ),
+      Container(
+        width: 30,
+        height: 30,
+        color: Colors.grey,
+        margin: EdgeInsets.all(2),
+      ),
+      Container(
+        width: 30,
+        height: 30,
+        color: Colors.black,
+        margin: EdgeInsets.all(2),
+      ),
+      Container(
+        width: 30,
+        height: 30,
+        color: Colors.white,
+        margin: EdgeInsets.all(2),
+      ),
+    ];
   }
 }
